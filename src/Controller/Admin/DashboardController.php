58671935b97025be982153aa4b4a8eef9a8e56a1
Nvmeless <alexandre.quilan.delaistre@gmail.com>
2025-04-29 14:01:01 +0200

    last from 2024

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -15,6 +15,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use App\Controller\Admin\PoolCompletionCrudController;
+use App\Repository\PlayerRepository;
+use App\Repository\PoolRepository;
+use App\Repository\SongRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
@@ -22,36 +25,39 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 class DashboardController extends AbstractDashboardController
 {
 
-       public function __construct(
+    public function __construct(
         private ChartBuilderInterface $chartBuilder,
-        private PoolCompletionRepository $poolCompletionRepository
+        private PoolCompletionRepository $poolCompletionRepository,
+        private PoolRepository $poolRepository,
+        private SongRepository $songRepository,
+        private PlayerRepository $playerRepository
     ) {
-
     }
-    private function downloadByDayChart(){
-            $period = new DatePeriod(
-        new DateTime('2024-06-24 18:03:27.000'),
-        new DateInterval('P1D'),
-        new DateTime(),
-    );
-    $dates = [];
-    $stats = [];
-    
-    foreach ($period as $key => $value) {
-    $dates[] = $value->format('Y-m-d');
+    private function rand_color()
+    {
+        return sprintf('#%06X', mt_rand(0, 0xFFFFFF));
     }
-    foreach($dates as $key => $value){
+    private function downloadByDayChart()
+    {
+        $period = new DatePeriod(
+            new DateTime('2024-06-17 00:00:01.000'),
+            new DateInterval('P1D'),
+            new DateTime(),
+        );
+        $dates = [];
+        $stats = [];
 
-        if(isset( $dates[$key + 1])){
-            $stats[] = $this->poolCompletionRepository->getStatistics($value, $dates[$key + 1])[0][1];
-
+        foreach ($period as $key => $value) {
+            $dates[] = $value->format('Y-m-d');
         }
-    }
+        foreach ($dates as $key => $value) {
 
-            // dd($stats);
-            // dd($pcomp);
-           $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
-        // ...set chart data and options somehow
+            if (isset($dates[$key + 1])) {
+                $stats[] = $this->poolCompletionRepository->getStatistics($value, $dates[$key + 1])[0][1];
+            }
+        }
+
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
 
         $chart->setData([
             'labels' => $dates,
@@ -60,7 +66,8 @@ class DashboardController extends AbstractDashboardController
                     'label' => 'Download',
                     'backgroundColor' => 'rgb(255, 99, 132)',
                     'borderColor' => 'rgb(255, 99, 132)',
-                    'data' =>$stats,
+                    'data' => $stats,
+                    'color' => "white"
                 ],
             ],
         ]);
@@ -75,41 +82,38 @@ class DashboardController extends AbstractDashboardController
         ]);
         return $chart;
     }
-
-
-        private function songRepartitionChart(){
-            $period = new DatePeriod(
-        new DateTime('2024-06-24 18:03:27.000'),
-        new DateInterval('P1D'),
-        new DateTime(),
-    );
-    $dates = [];
-    $stats = [];
-    
-    foreach ($period as $key => $value) {
-    $dates[] = $value->format('Y-m-d');
-    }
-    foreach($dates as $key => $value){
-
-        if(isset( $dates[$key + 1])){
-            $stats[] = $this->poolCompletionRepository->getStatistics($value, $dates[$key + 1])[0][1];
-
+    private function downloadByPoolsChart()
+    {
+        $poolsEntities = $this->poolRepository->findAll();
+        $pools = [];
+        $poolarr = [];
+        foreach ($poolsEntities as $key => $pool) {
+            $pools[$pool->getId()] = $pool->getName();
+            $poolarr[] = $pool->getName();
         }
-    }
+        $stats = [];
+        $colors = [];
+        foreach ($pools as $poolId => $pool) {
+            $stats[] = count($this->poolCompletionRepository->findBy(["pool" => $poolId]));
+            $colors[] = $this->rand_color();
+        }
 
-            // dd($stats);
-            // dd($pcomp);
-           $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
-        // ...set chart data and options somehow
+
+        return $this->generatePolarChart(['indexes' => $poolarr, "stats" => $stats, "colors" => $colors]);
+    }
+    private function generatePolarChart($datas)
+    {
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_POLAR_AREA);
 
         $chart->setData([
-            'labels' => $dates,
+            'labels' => $datas["indexes"],
             'datasets' => [
                 [
                     'label' => 'Download',
-                    'backgroundColor' => 'rgb(255, 99, 132)',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' =>$stats,
+                    'backgroundColor' => $datas["colors"],
+                    'borderColor' => $datas["colors"],
+                    'data' => $datas["stats"],
+
                 ],
             ],
         ]);
@@ -118,33 +122,38 @@ class DashboardController extends AbstractDashboardController
             'scales' => [
                 'y' => [
                     'suggestedMin' => 0,
-                    'suggestedMax' => max($stats) + 5,
+                    'suggestedMax' => max($datas["stats"]) + 5,
                 ],
             ],
         ]);
         return $chart;
+    }
+    private function songRepartitionChart()
+    {
+
+        $songsEntities = $this->songRepository->findAll();
+        $songs = [];
+        $songarr = [];
+        foreach ($songsEntities as $key => $song) {
+            $songs[$song->getId()] = $song->getName();
+            $songarr[] = $song->getName();
+        }
+
+        $stats = [];
+        $colors = [];
+        foreach ($songs as $songId => $song) {
+            $stats[] = count($this->poolCompletionRepository->findBy(["song" => $songId]));
+            $colors[] = $this->rand_color();
+        }
+        return $this->generatePolarChart(['indexes' => $songarr, "stats" => $stats, "colors" => $colors]);
     }
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        // return parent::index();
+        $countDownload = count($this->poolCompletionRepository->findAll());
+        $countPeople = count($this->playerRepository->findAll());
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(PoolCompletionCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-          
-
-        return $this->render('admin/dashboard.html.twig', ["downloadByDayChart" => $this->downloadByDayChart(),"songRepartitionChart" => $this->songRepartitionChart()]);
+        return $this->render('admin/dashboard.html.twig', ["countDownload" => $countDownload, "countPlayers" => $countPeople, "downloadByPoolsChart" => $this->downloadByPoolsChart(), "downloadByDayChart" => $this->downloadByDayChart(), "songRepartitionChart" => $this->songRepartitionChart(), ""]);
     }
 
     public function configureDashboard(): Dashboard
@@ -156,30 +165,20 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
 
-           
         return [
             MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
 
             MenuItem::section('FDLM'),
-            MenuItem::linkToCrud('Pool Completion', 'fa fa-tags', PoolCompletion::class),
-            // MenuItem::linkToCrud('Blog Posts', 'fa fa-file-text', BlogPost::class),
-
-            // MenuItem::section('Users'),
-            // MenuItem::linkToCrud('Comments', 'fa fa-comment', Comment::class),
-            // MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
+            // MenuItem::linkToCrud('Pool Completion', 'fa fa-tags', PoolCompletion::class),
         ];
-    
-
-        // yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 
     public function configureAssets(): Assets
-{
-    $assets = parent::configureAssets();
+    {
+        $assets = parent::configureAssets();
 
-    $assets->addWebpackEncoreEntry('app');
-
-    return $assets;
-}
+        $assets->addWebpackEncoreEntry('app');
+        $assets->addCssFile('css/admin.css');
+        return $assets;
+    }
 }
